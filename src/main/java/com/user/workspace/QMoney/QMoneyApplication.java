66c690ad351807eb859543ab03fad87d4a914235
Manish Verma <manish.verma217@gmail.com>
2023-07-31 09:39:59 +0530

    First Commit with empty source file structure

---
+++ src/main/java/com/user/workspace/QMoney/QMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.user.workspace.QMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QMoneyApplication.class, args);
+	}
+
+}
